cc01b46ddd6987d8fb4313878695ef9794a26990
Caleb Campbell <caleb@builtbyhq.com>
2023-07-29 10:43:45 -0600

    centered images

--- src/components/ServicesPanels.tsx
+++ src/components/ServicesPanels.tsx
@@ -9,17 +9,17 @@ export const ServicesPanels = ({
 }) => {
   return (
     <>
-      <div className="mx-auto w-10/12 max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <p className="m-3 mx-auto mb-10 w-full p-3 text-center text-3xl uppercase md:text-4xl ">
+      <div className=" w-full mx-auto max-w-screen-xl sm:px-6 lg:px-8">
+        <p className=" mb-10 w-full p-3 text-center text-3xl uppercase md:text-4xl ">
           {title}
         </p>
-        <div className="flex flex-col justify-around sm:flex-row lg:gap-[30px]">
+        <div className="flex mx-auto flex-col w-10/12 justify-around items-center sm:flex-row lg:gap-[30px]">
           {images.map((image) => {
             return (
               <Link
                 href={image.path}
                 key={image.text}
-                className="relative my-10 flex-shrink-0 cursor-pointer transition-all hover:scale-105 hover:brightness-75 hover:filter"
+                className="relative my-10 mx-auto flex-shrink-0 cursor-pointer transition-all hover:scale-105 hover:brightness-75 hover:filter"
               >
                 <img
                   className="w-72 h-96 hover:blur-sm "
